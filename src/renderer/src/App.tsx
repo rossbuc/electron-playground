0d6e948ad51c18642dab42d1f3bd65de8d2ac422
Ross Buchan <rossbuchan1@googlemail.com>
2023-11-22 13:03:37 +0000

    Get file explorer from other app working

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,21 @@
 import Versions from "./components/Versions"
+import { useState } from "react"
 
 function App(): JSX.Element {
+  const [filePath, setFilePath] = useState<string>("")
+
+  const handleOnClick = async (): Promise<void> => {
+    const newFilePath = await window.electron.ipcRenderer.invoke("dialog:openFile")
+    setFilePath(newFilePath)
+  }
+
   return (
     <>
       <Versions />
+      <button onClick={handleOnClick}>Upload a file</button>
+      <h1>
+        the file path is <strong>{filePath}</strong>
+      </h1>
     </>
   )
 }

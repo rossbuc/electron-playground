7458d5760d0f6161ab816c10ca912943bcb77239
Ross Buchan <rossbuchan1@googlemail.com>
2024-01-31 10:51:54 +0100

    Fix song rendering list and successfuly get howler to play/pause tracks

--- src/renderer/src/components/DataDisplay.tsx
+++ src/renderer/src/components/DataDisplay.tsx
@@ -1,25 +1,46 @@
 import { Howl, Howler } from "howler"
+import { useEffect, useState } from "react"
 
 const DataDisplay = ({ library }): JSX.Element => {
+  const [playingSong, setPlayingSong] = useState(undefined)
+  useEffect(() => {
+    playingSong ? playingSong.play() : console.log("playing song is undefined")
+  }, [playingSong])
   console.log("The array is an array true/false, ", Array.isArray(library))
+  const handlePause: void = () => {
+    playingSong ? playingSong.pause() : console.log("there is no song playing")
+  }
+
   const songNodes = library
     ? library.map((song, index) => (
-        <button key={index} onClick={handleSongClick(song)}>
+        <button
+          key={index}
+          onClick={() =>
+            setPlayingSong(
+              new Howl({
+                src: [song.songPath],
+                onend: setPlayingSong(undefined)
+              })
+            )
+          }
+        >
           {song.songMetaData.title}
         </button>
       ))
     : "loading"
 
   console.log(songNodes)
-  return <>{songNodes}</>
+  return (
+    <>
+      <button onClick={handlePause}>Pause</button>
+      {songNodes}
+    </>
+  )
 }
 
-const handleSongClick: void = (song) => {
-  // play the song
-  let playingSong = new Howl({
-    src: [song.songPath]
-  })
-  playingSong.play()
-}
+// const handleSongPlay: void = (song) => {
+//   // play the song
+//   song.play()
+// }
 
 export default DataDisplay
